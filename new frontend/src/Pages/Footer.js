074b2855a1691f0a38bad038c09d1bf0578b44f7
Adetunji Samuel <125776151+sam-aydev@users.noreply.github.com>
2026-04-22 18:00:18 +0100

    Refactor Footer component with updated structure and links

--- new frontend/src/Pages/Footer.js
+++ new frontend/src/Pages/Footer.js
@@ -1,148 +1,61 @@
-// import Frame from "../images/Frame19.png";
-// import Frame1 from "../images/Frame20.png";
-// import Frame2 from "../images/Frame21.png";
 
-// function Footer() {
-//   return (
-//     <div className="footer" id="contact">
-//       <div className="footer1">
-//         <div>
-//           <div className="ffree">FreelancerAI</div>
-//           <div className="ffree1">the working AI proposal</div>
-//           <div className="ffree2">
-//             <div>
-//               <img src={Frame} alt="Frame" />
-//             </div>
-//             <div>
-//               <img src={Frame1} alt="Frame" />
-//             </div>
-//             <div>
-//               <img src={Frame2} alt="Frame" />
-//             </div>
-//           </div>
-//         </div>
-
-//         <div>
-//           <div className="fquick">Quicklinks</div>
-//           <div className="flinks1">
-//             <a href="#">
-//               <li>Home</li>
-//             </a>
-//             <a href="#how-it-works">
-//               <li>Features</li>
-//             </a>
-//             <a href="#how-it-works">
-//               <li>how it works</li>
-//             </a>
-//             <a href="#how-it-works">
-//               <li>Use Case</li>
-//             </a>
-//           </div>
-//         </div>
-
-//         <div>
-//           <div className="fquick">Support</div>
-//           <div className="flinks1">
-//             <a href="#how-it-works">
-//               <li>Contact</li>
-//             </a>
-//             <a href="#how-it-works">
-//               <li>Help Center</li>
-//             </a>
-//             <a href="#how-it-works">
-//               <li>Email</li>
-//             </a>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="footer2">
-//         <p>@ BuildON Inc. All rights reserved.</p>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Footer;
-
-'use client'
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  FaInstagram,
-  FaLinkedin,
-  FaXTwitter,
-  FaEnvelope,
-} from 'react-icons/fa6' // Using Fa6 for the latest X (Twitter) icon
+  faXTwitter
+} from '@fortawesome/free-brands-svg-icons'
 import './Footer.css'
 
 function Footer() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className='footer-section' id='contact'>
-      <div className='footer-container'>
-        {/* Brand Section */}
+    <footer className='footer'>
+      <div className='footer-top'>
+        {/* Left Section: Brand & Socials */}
         <div className='footer-brand'>
-          <h2 className='brand-logo'>FreelancerAI</h2>
-          <p className='brand-tagline'>The Working AI Proposal</p>
+          <div className='brand-logo'>
+            minimals
+          </div>
+          <p className='brand-tagline'>Precision document intelligence.</p>
           <div className='social-links'>
-            <a href='#' aria-label='Instagram' className='social-icon'>
-              <FaInstagram size={18} />
+            
+            <a
+              href='https://x.com/0xminimals?s=2'
+              aria-label='Twitter'
+              className='social-icon'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FontAwesomeIcon icon={faXTwitter} />
             </a>
-            <a href='#' aria-label='X (Twitter)' className='social-icon'>
-              <FaXTwitter size={18} />
-            </a>
-            <a href='#' aria-label='LinkedIn' className='social-icon'>
-              <FaLinkedin size={18} />
-            </a>
+            
           </div>
         </div>
 
-        {/* Quick Links */}
-        <div className='footer-links-group'>
-          <h3 className='links-title'>Quick Links</h3>
-          <ul className='footer-links'>
-            <li>
-              <a href='#'>Home</a>
-            </li>
+        {/* Right Section: Quick Links */}
+        <div className='footer-links-container'>
+          <h3>Quick Links</h3>
+          <ul className='footer-nav'>
             <li>
               <a href='#features'>Features</a>
             </li>
             <li>
-              <a href='#how-it-works'>How it Works</a>
+              <a href='#how'>How It Works</a>
             </li>
             <li>
-              <a href='#use-cases'>Use Case</a>
-            </li>
-          </ul>
-        </div>
-
-        {/* Support Section */}
-        <div className='footer-links-group'>
-          <h3 className='links-title'>Support</h3>
-          <ul className='footer-links'>
-            <li>
-              <a href='#contact'>Contact</a>
+              <a href='#who'>Who is it for</a>
             </li>
             <li>
-              <a href='#help'>Help Center</a>
-            </li>
-            <li className='email-link'>
-              <a href='mailto:support@buildon.org'>
-                <FaEnvelope size={14} />
-                <span>support@buildon.org</span>
-              </a>
+              <a href='#upload'>Get Started</a>
             </li>
           </ul>
         </div>
       </div>
 
+      {/* Bottom Section: Copyright */}
       <div className='footer-bottom'>
         <p>© {currentYear} BuildON Inc. All Rights Reserved.</p>
-        <div className='footer-legal'>
-          <a href='/privacy'>Privacy Policy</a>
-          <span className='separator'>•</span>
-          <a href='/terms'>Terms of Service</a>
-        </div>
       </div>
     </footer>
   )
